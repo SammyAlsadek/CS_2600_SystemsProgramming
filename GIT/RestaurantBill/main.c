164f99d6c7a871c358c46ec2e9d21464600a49ae
Sammy Alsadek <sammyalsadek@Sammys-MacBook-Air.local>
2021-09-29 19:44:12 -0700

    Initial commit for 'Restaurant Bill' program. SA

---
+++ GIT/RestaurantBill/main.c
@@ -0,0 +1,8 @@
+//=====================================================================================================
+// Name: Sammy Alsadek
+// File: main.c
+// Date: Sep 29th 2021, 7:41PM
+// Description: This is a program that will compute the tax and tip on a restaurant bill for a patron. 
+//              The program should accept the tax and tip both as percentages from the command line. 
+//              Display the meal cost, tax amount, tip amount, and total bill on the screen.
+//=====================================================================================================
