877367f9c9431f96edf64dfb8f1f1bb9209ab348
Sammy Alsadek <sammyalsadek@Sammys-MacBook-Air.local>
2021-09-29 21:22:20 -0700

    Initial commit for 'Number Guessing Game' program. SA

---
+++ GIT/NumberGuessingGame/main.c
@@ -0,0 +1,6 @@
+//=====================================================================================================
+// Name: Sammy Alsadek
+// File: main.c
+// Date: Sep 29th 2021, 9:16PM
+// Description: An interactive program that allows a user to try and guess a randomly generated number
+//=====================================================================================================
